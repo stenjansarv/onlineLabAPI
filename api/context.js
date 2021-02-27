93933fd5e22caa4590d08102cfe7a8a1180faee5
stenjansarv <stenjansarv@gmail.com>
2021-02-27 15:09:18 +0200

    chore: add helper methods

---
+++ api/context.js
@@ -0,0 +1,8 @@
+const contextualize = require('../lib/context')
+
+module.exports = (event) => {
+  const { publisherId, publicationId } = event.pathParameters
+  const context = contextualize({ publisherId, publicationId })
+
+  return context
+}

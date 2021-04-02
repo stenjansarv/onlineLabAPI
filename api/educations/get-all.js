1588cac9a1061a8ae86e387e4a48e95a8c461a7e
stenjansarv <stenjansarv@gmail.com>
2021-04-02 17:36:41 +0300

    fix: add safety net when not importing publications

--- api/educations/get-all.js
+++ api/educations/get-all.js
@@ -1,12 +1,51 @@
+const moment = require('moment')
+const _ = require('lodash')
+const fetch = require('node-fetch')
 const contextualize = require('../context')
 const { success, invalid } = require('../respond')
 const serialize = require('../serializers/education')
 
 const fetchAll = require('../../lib/finders/educations/fetch-all')
 
+const mapEducationToObject = (education, context) => {
+  const orgCity = _.get(education['summaries'][0]['education-summary'], 'organization.address.city', null)
+  const orgRegion = _.get(education['summaries'][0]['education-summary'], 'organization.address.region', null)
+  const orgCountry = _.get(education['summaries'][0]['education-summary'], 'organization.address.country', null)
+  const address = [orgCity, orgRegion, orgCountry].filter(item => item !== null)
+
+  return _.omitBy({
+    '@timestamp': moment().valueOf(),
+    publisher_id: context.publisherId,
+    education_id: _.get(education['summaries'][0]['education-summary'], 'put-code'),
+    start_date: _.get(education['summaries'][0]['education-summary'], 'start-date.year.value', null) === null ? null : moment(`${_.get(education['summaries'][0]['education-summary'], 'start-date.year.value', '0000')}-${_.get(education['summaries'][0]['education-summary'], 'start-date.month.value', '01')}-${_.get(education['summaries'][0]['education-summary'], 'start-date.day.value', '01')}`).valueOf(),
+    end_date: _.get(education['summaries'][0]['education-summary'], 'end-date.year.value', null) === null ? null : moment(`${_.get(education['summaries'][0]['education-summary'], 'end-date.year.value', '0000')}-${_.get(education['summaries'][0]['education-summary'], 'end-date.month.value', '01')}-${_.get(education['summaries'][0]['education-summary'], 'end-date.day.value', '01')}`).valueOf(),
+    last_modified_date: _.get(education['summaries'][0]['education-summary'], 'last-modified-date.value'),
+    role_title: _.get(education['summaries'][0]['education-summary'], 'role-title'),
+    organization_name: _.get(education['summaries'][0]['education-summary'], 'organization.name'),
+    organization_address: `${address.join(', ')}`,
+  }, _.isUndefined)
+}
+
 exports.handler = async (event) => {
   const context = contextualize(event)
   const result = await fetchAll(context)
+
+  if (_.isEmpty(result)) {
+    const response = await fetch(`https://pub.orcid.org/v3.0/${context.publisherId}/educations`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const body = await response.json()
+
+    const importedEducations = body['affiliation-group']
+  
+    const payload = _.map(importedEducations, education => mapEducationToObject(education, context))
+
+    return success(payload)
+  }
 
   if (result.statusCode === 400) return invalid(result)
 
--- api/employments/get-all.js
+++ api/employments/get-all.js
@@ -1,12 +1,52 @@
+const moment = require('moment')
+const _ = require('lodash')
+const fetch = require('node-fetch')
 const contextualize = require('../context')
 const { success, invalid } = require('../respond')
 const serialize = require('../serializers/employment')
 
 const fetchAll = require('../../lib/finders/employments/fetch-all')
 
+const mapEmploymentToObject = (employment, context) => {
+  const orgCity = _.get(employment['summaries'][0]['employment-summary'], 'organization.address.city', null)
+  const orgRegion = _.get(employment['summaries'][0]['employment-summary'], 'organization.address.region', null)
+  const orgCountry = _.get(employment['summaries'][0]['employment-summary'], 'organization.address.country', null)
+  const address = [orgCity, orgRegion, orgCountry].filter(item => item !== null)
+
+  return _.omitBy({
+    '@timestamp': moment().valueOf(),
+    publisher_id: context.publisherId,
+    employment_id: _.get(employment['summaries'][0]['employment-summary'], 'put-code'),
+    role_title: _.get(employment['summaries'][0]['employment-summary'], 'role-title'),
+    department_name: _.get(employment['summaries'][0]['employment-summary'], 'department-name'),
+    start_date: _.get(employment['summaries'][0]['employment-summary'], 'start-date.year.value', null) === null ? null : moment(`${_.get(employment['summaries'][0]['employment-summary'], 'start-date.year.value')}-${_.get(employment['summaries'][0]['employment-summary'], 'start-date.month.value', '01')}-${_.get(employment['summaries'][0]['employment-summary'], 'start-date.day.value', '01')}`).valueOf(),
+    end_date: _.get(employment['summaries'][0]['employment-summary'], 'end-date.year.value', null) === null ? null : moment(`${_.get(employment['summaries'][0]['employment-summary'], 'end-date.year.value')}-${_.get(employment['summaries'][0]['employment-summary'], 'end-date.month.value', '01')}-${_.get(employment['summaries'][0]['employment-summary'], 'end-date.day.value', '01')}`).valueOf(),
+    last_modified_date: _.get(employment['summaries'][0]['employment-summary'], 'last-modified-date.value'),
+    organization_name: _.get(employment['summaries'][0]['employment-summary'], 'organization.name', null),
+    organization_address: `${address.join(', ')}`,
+    organization_url: _.get(employment['summaries'][0]['employment-summary'], 'url.value', null),
+  }, _.isUndefined)
+}
+
 exports.handler = async (event) => {
   const context = contextualize(event)
   const result = await fetchAll(context)
+
+  if (_.isEmpty(result)) {
+    const response = await fetch(`https://pub.orcid.org/v3.0/${context.publisherId}/employments`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const body = await response.json()
+
+    const importedEmployments = body['affiliation-group']
+  
+    const payload = _.map(importedEmployments, employment => mapEmploymentToObject(employment, context))
+    return success(payload)
+  }
 
   if (result.statusCode === 400) return invalid(result)
 
